13b9758dc55dc395371a5b8348ab8d8d831af9ff
DrewDalmedo <andrewdalmedo@gmail.com>
2020-04-14 17:06:54 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+require 'pry'
+# Add code here!
+def prime?(num)
+    return false if num <= 1
+    (2..num-1).each do |i|
+        return false if num % i == 0
+    end
+
+    return true
+end
