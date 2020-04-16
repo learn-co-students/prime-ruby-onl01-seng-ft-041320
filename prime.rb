b6ece5fec0410331d2b0608f93a9bca9c1e37134
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-16 15:19:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
 def prime? (num)
-  return false if num%2 == 0
-  attempts = (3..Integer.sqrt(num)).collect {|odd| odd %2 !=0}
-  
+  return true if num == 2
+  return false if num%2 == 0 || num.abs != num || num == 1
+  attempts = (3..Integer.sqrt(num)).to_a
+  odds = attempts.select {|odd| odd %2 != 0}
+  if odds.any? {|odd| num % odd == 0}
+    return false
+  else
+    return true 
+  end
 end
