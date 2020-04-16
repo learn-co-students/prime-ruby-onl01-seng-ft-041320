b66b32d8cd7b7362ffced0aeba5eeb219bd8bd19
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-16 13:43:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 # Add  code here!
 
 
-
 def prime?(num)
-  return false if num < 2
-
-  (2..num/2).none?{|i| num % i == 0}
+    if num < 2 
+    return false
+  else
+    (2..num-1).to_a.all? do |check|
+      num % check != 0
+    end
+  end
 end
-  
