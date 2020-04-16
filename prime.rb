0171358fc9bc1674d48e2bf9512616c33d53fa1d
Lucas Easley <lucaseasley11@gmail.com>
2020-04-16 19:39:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+  prime_number = (2..n-1).to_a
+    if n < 2
+      return false
+    else prime_number.each do |integer|
+      if n % integer == 0
+        return false
+        end
+      end
+    end
+  return true
+end
