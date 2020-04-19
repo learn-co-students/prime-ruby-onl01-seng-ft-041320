393caeec53c67cfb4c177019d1a757c0f8653739
Michael Piercy <michael.piercy@zoho.com>
2020-04-19 17:56:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+  return false
+  end
+  i = 2
+  while i < int
+  return false if int % i == 0
+  i += 1
+  end
+  false
+  true
+end
+
+prime?(2)
+prime?(-1)
