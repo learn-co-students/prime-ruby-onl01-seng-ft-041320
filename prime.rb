e563b1df0fa249d23bd547d6255fb8a66a3fe7ca
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-20 15:05:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(a)
+  if (a <= 1)
+    return false
+ else
+    (2..a-1).to_a.all? do |possible_factor|
+      a % possible_factor != 0
+    end
+ 
+end# Add  code here!
+end
